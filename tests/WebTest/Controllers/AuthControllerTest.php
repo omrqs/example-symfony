ee8d76aac18469b49fbd2bc2b4c57f6a9a17ad38
Otávio Marques <otaviomarques@gmail.com>
2020-07-17 16:12:34 -0300

    C uncomment auth success test.

--- tests/WebTest/Controllers/AuthControllerTest.php
+++ tests/WebTest/Controllers/AuthControllerTest.php
@@ -5,34 +5,34 @@ use App\Tests\AbstractCoreTest;
 
 class AuthControllerTest extends AbstractCoreTest
 {
-    // /**
-    //  * Test session logged.
-    //  */
-    // public function testAuthSuccess()
-    // {
-    //     $this->client->xmlHttpRequest(
-    //         'GET',
-    //         $this->router->generate('auth_check'),
-    //         [],
-    //         [],
-    //         self::$loggedHeaders
-    //     );
+    /**
+     * Test session logged.
+     */
+    public function testAuthSuccess()
+    {
+        $this->client->xmlHttpRequest(
+            'GET',
+            $this->router->generate('auth_check'),
+            [],
+            [],
+            self::$loggedHeaders
+        );
 
-    //     $response = $this->client->getResponse();
+        $response = $this->client->getResponse();
 
-    //     $this->assertSame(200, $response->getStatusCode());
+        $this->assertSame(200, $response->getStatusCode());
 
-    //     $this->assertTrue(
-    //         $response->headers->contains(
-    //             'Content-Type',
-    //             'application/json'
-    //         )
-    //     );
+        $this->assertTrue(
+            $response->headers->contains(
+                'Content-Type',
+                'application/json'
+            )
+        );
 
-    //     ['messages' => $messages] = json_decode($response->getContent(), true);
+        ['messages' => $messages] = json_decode($response->getContent(), true);
 
-    //     $this->assertArrayHasKey('info', $messages);
-    // }
+        $this->assertArrayHasKey('info', $messages);
+    }
 
     /**
      * Test session not logged.
